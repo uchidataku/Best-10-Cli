d9002c2919617aa3a5ef3d77cbc36f8f19bb7588
uchidataku <bbb.rchp99@gmail.com>
2022-10-31 10:20:51 +0900

    feat: SignUpPage仮実装

--- src/components/pages/SignUpPage/SignUp/index.tsx
+++ src/components/pages/SignUpPage/SignUp/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import axios from "../../../../config/axios";
+import Api from "../../../../config/qpi";
+import { Input, Button } from 'antd';
 
 const SignUp = () => {
-    return (
-        <div>
-            <p>
-                新規登録
-            </p>
-        </div>
-    );
+  async function onClickSignUp() {
+    const request = await axios.post(Api.signUp.buildPath());
+    return request;
+  }
+
+  return (
+    <div>
+      <p>新規登録</p>
+      <Input placeholder="Username" />
+      <Input.Password placeholder="Password" />
+
+      <Button
+        type="primary"
+        onClick={() => onClickSignUp()}
+      >
+        登録
+      </Button>
+    </div>
+  );
 };
 
 export default SignUp;
