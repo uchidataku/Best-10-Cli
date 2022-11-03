818248c9a846eb7748f32ce04039e8f40c93bcab
uchidataku <bbb.rchp99@gmail.com>
2022-11-03 23:42:31 +0900

    feat: searchBar仮実装

--- src/components/pages/TopPage/Top/index.tsx
+++ src/components/pages/TopPage/Top/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import RankingList from "../../../molecules/RankingList";
+import { SearchBar } from "antd-mobile";
+import styles from "./style.module.scss";
 
 const Top = () => {
   return (
     <div className="App">
+      <SearchBar className={styles.searchBar} placeholder="キーワード" />
       <RankingList />
     </div>
   );
