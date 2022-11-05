fce7e3542d5f821114ffcaeb970d12f6b499aff5
uchidataku <bbb.rchp99@gmail.com>
2022-11-05 13:58:20 +0900

    feat: 仮実装

--- src/config/App.tsx
+++ src/config/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import PageRoutes from "./PageRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import styles from "./style.module.scss";
+import { MenuOutlined } from "@ant-design/icons";
 
 function App() {
   return (
     <div className={styles.app}>
       <header className={styles.header}>
         <h2>Best-10</h2>
+        <MenuOutlined className={styles.menuIcon}/>
       </header>
       <main>
         <Router>
