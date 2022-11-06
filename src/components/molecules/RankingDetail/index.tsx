e07d649048654915648cd6377b3f4dc7578d5ec0
uchidataku <bbb.rchp99@gmail.com>
2022-11-06 12:19:01 +0900

    feat: Item追加機能の実装

--- src/components/molecules/RankingDetail/index.tsx
+++ src/components/molecules/RankingDetail/index.tsx
@@ -7,24 +7,51 @@ import Account from "../../../models/Account";
 import Item from "../../../models/Item";
 import Ranking from "../../../models/Ranking";
 import { genreLabelFor } from "../../../models/Ranking/helpers";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { Toast } from "antd-mobile";
 
 type Props = {
   rankingId: string;
 };
 
+type FormInput = {
+  name: string;
+}
+
 const RankingDetail = ({ rankingId }: Props) => {
   const [ranking, setRanking] = useState<Ranking>();
   const [creator, setCreator] = useState<Account>();
   const [items, setItems] = useState<Item[]>([]);
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+  const onSubmit = (data: FormInput) => {
+    axios
+      .post(Api.createRankingItem.buildPath(rankingId), {
+        item: { ...data }
+      })
+      .then((res) => {
+        console.log(res.data);
+        fetchData();
+        reset();
+        Toast.show({
+          icon: "success",
+          content: "追加しました",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  async function fetchData() {
+    const request = await axios.get(Api.fetchRanking.buildPath(rankingId));
+    setRanking(request.data);
+    setCreator(request.data.creator);
+    setItems(request.data.items);
+    return request;
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      const request = await axios.get(Api.fetchRanking.buildPath(rankingId));
-      setRanking(request.data);
-      setCreator(request.data.creator);
-      setItems(request.data.items);
-      return request;
-    }
     fetchData();
   }, []);
 
@@ -44,6 +71,14 @@ const RankingDetail = ({ rankingId }: Props) => {
           <RankingDetailItem key={item.id} rank={idx + 1} item={item} />
         ))}
       </div>
+      <form className={styles.addItem} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className={styles.addItemInput}
+          placeholder={ranking?.title}
+          {...register("name")}
+        />
+        <Button className={styles.addItemButton} onClick={handleSubmit(onSubmit)}>追加する</Button>
+      </form>
     </div>
   );
 };
