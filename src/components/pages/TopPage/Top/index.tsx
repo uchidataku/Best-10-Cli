cd23f9c5ea7ba60af2153463383beffc0f814d69
uchidataku <bbb.rchp99@gmail.com>
2023-01-15 13:00:26 +0900

    feat: Topページをシンプルに

--- src/components/pages/TopPage/Top/index.tsx
+++ src/components/pages/TopPage/Top/index.tsx
@@ -4,48 +4,19 @@ import styles from "./style.module.scss";
 import Ranking from "../../../../models/Ranking";
 import axios from "../../../../config/axios";
 import Api from "../../../../config/qpi";
-import { Button } from "antd";
 import { ContentOutline } from "antd-mobile-icons";
 import Select from "react-select";
 import { SortByObjects } from "../../../../models/Ranking/helpers";
-import GenreCheckboxModal from "../../../molecules/GenreCheckboxModal";
-import BackDrop from "../../../molecules/BackDrop";
-import classNames from "classnames";
-
-// type QueryInput = {
-//   keyword?: string;
-//   genre?: { value: string; label: string };
-//   sortBy?: { value: string; label: string };
-// };
-
-// type QueryInput = {
-//   keyword?: string;
-//   genreIds: string[];
-//   sortBy?: { value: string; label: string };
-// };
 
 const Top = () => {
   const [rankings, setRankings] = useState<Ranking[]>([]);
   const [rankingsCount, setRankingsCount] = useState(0);
-  const [openGenreModal, setOpenGenreModal] = useState(false);
   const defaultSortByParams = SortByObjects[0];
-  const closeSideDrawer = (): void => {
-    setOpenGenreModal(false);
-  };
-  const onCheckGenreIds = ({ ids }: { ids: string[] }): void => {
-    setGenreIds(ids);
-  };
 
-  const [keyword, setKeyword] = useState<string>();
-  const [genreIds, setGenreIds] = useState<string[]>([]);
-  const [sortBy, setSortBy] = useState("popularity");
-
-  const onSubmit = () => {
+  const onSubmit = (sortBy: string) => {
     axios
       .get(Api.fetchRankings.buildPath(), {
         params: {
-          keyword: keyword,
-          genreIds: genreIds,
           sortBy: sortBy,
         },
       })
@@ -84,32 +55,13 @@ const Top = () => {
   return (
     <div>
       <div className={styles.form}>
-        <input
-          className={styles.searchBar}
-          onChange={(e) => setKeyword(e.target.value)}
-          value={keyword}
-          type="text"
-          placeholder="キーワード"
-        />
-        <div
-          className={classNames(styles.genreInput, { [styles.active]: genreIds.length })}
-          onClick={() => setOpenGenreModal(true)}
-        >
-          <p>ジャンル{genreIds.length > 0 && " ・ " + genreIds.length}</p>
-        </div>
-
         <Select
           className={styles.sortByInput}
-          onChange={(e) => (e !== null ? setSortBy(e.value) : null)}
+          onChange={(e) => (e !== null ? onSubmit(e.value) : null)}
           defaultValue={defaultSortByParams}
           options={SortByObjects}
         />
-        <Button className={styles.searchButton} onClick={() => onSubmit()}>
-          検索
-        </Button>
       </div>
-      <GenreCheckboxModal onCheck={onCheckGenreIds} isOpen={openGenreModal} />
-      {openGenreModal && <BackDrop closeSideDrawer={closeSideDrawer} />}
       <div className={styles.rankingsCount}>
         <ContentOutline /> {rankingsCount}
       </div>
